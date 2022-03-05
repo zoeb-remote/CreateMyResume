7b87a92c038a75d9af36f7477f43011952b308b2
Shoeb Idris <shoeb@xam.com.au>
2022-03-05 10:54:11 +0530

    Initial Commit

---
+++ CreateMyResume/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CreateMyResume
+//
+//  Created by Bigsur on 05/03/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
