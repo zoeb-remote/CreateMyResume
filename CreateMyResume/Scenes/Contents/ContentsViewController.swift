891ddc67017b03d49daa84c91de372999e782605
Zoeb <zoeb.remote.iosdevloper@gmail.com>
2022-03-05 13:36:11 +0530

    Add Contents scene with basic functionality

---
+++ CreateMyResume/Scenes/Contents/ContentsViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ContentsViewController.swift
+//  CreateMyResume
+//
+//  Created by Zoeb on 05/03/22.
+//
+
+import UIKit
+
+class ContentsViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView!
+    
+    private lazy var viewModel = ContentsViewModel()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
+//MARK: - UITableViewDataSource and UITableViewDelegate
+
+extension ContentsViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        viewModel.records.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: viewModel.cellIdentifier, for: indexPath)
+        
+        //Default Content Configuration
+        var content = cell.defaultContentConfiguration()
+        content.text = viewModel.records[indexPath.item]
+        
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+}
---
+++ CreateMyResume/Scenes/Contents/ContentsViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ContentsViewModel.swift
+//  CreateMyResume
+//
+//  Created by Zoeb on 05/03/22.
+//
+
+import Foundation
+
+struct ContentsViewModel {
+    let records = ["About", "Contact", "Career Objective", "Work", "Skills", "Education", "Project Details"]
+    let cellIdentifier = "ContentsTableViewCell"
+}
